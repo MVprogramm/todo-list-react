f7a07a5a3a82da9668be2cfb159f6f5ceba53648
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-05-29 09:26:55 +0300

    add input and button

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./styles.css";
 
 const rootElem = document.querySelector("#root");
 
 const titleElem = <h1 className="title">Todo List</h1>;
+const todoListElem = (
+  <main className="todo-list">
+    <div className="actions">
+        <input className="task-input" type="text" />
+        <button className="btn create-task-btn">Create</button>
+    </div>
+    <ul className="list"></ul>
+  </main>
+);
 
-ReactDOM.render(titleElem, rootElem);
+
+const todoElem = (
+  <>
+    {titleElem}
+    {todoListElem}
+  </>
+)
+
+ReactDOM.render(todoElem, rootElem);
