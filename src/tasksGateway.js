8ec427c91727e71a572513ceb859bb652a904c70
MichaelValiahmetov <37244309+MVprogramm@users.noreply.github.com>
2022-06-27 10:12:53 +0300

    Update tasksGateway.js

--- src/tasksGateway.js
+++ src/tasksGateway.js
@@ -6,7 +6,7 @@ export const createTask = taskData =>
   fetch(baseURL, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json; charset=utf-8',
+      'Content-Type': 'application/json;utf-8',
     },
     body: JSON.stringify(taskData),
   }).then(res => {
